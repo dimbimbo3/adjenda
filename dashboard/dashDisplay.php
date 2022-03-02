8425c6c5d7d4a8b375738e5e21b87679f734edcb
Dan Imbimbo <dimbimbo@student.sussex.edu>
2022-03-01 21:36:40 -0500

    Formatting of main tag

--- dashboard/dashDisplay.php
+++ dashboard/dashDisplay.php
@@ -1,27 +1,26 @@
-<main>
-
 <!--Includes the defined header-->
 <?php include '../view/header.php'; ?>
 
-<div class="container">
-    <h1>Successful login</h1>
-    <h3>Account Email: <?php echo $_SESSION["accEmail"]?></h3>
-    <h3>Account Type: 
-        <?php
-            if($_SESSION["accType"] == "S"){
-                echo "Student";
-            }
-            else if($_SESSION["accType"] == "I"){
-                echo "Instructor";
-            }
-            else{
-                    echo "ERROR! No Account Type.";
-                    exit;
-            }
-         ?>
-    </h3>
-</div>
-<a href="../courses/courses.php">Placeholder Class Link</a>
+<main>
+    <div class="container">
+        <h1>Successful login</h1>
+        <h3>Account Email: <?php echo $_SESSION["accEmail"]?></h3>
+        <h3>Account Type: 
+            <?php
+                if($_SESSION["accType"] == "S"){
+                    echo "Student";
+                }
+                else if($_SESSION["accType"] == "I"){
+                    echo "Instructor";
+                }
+                else{
+                        echo "ERROR! No Account Type.";
+                        exit;
+                }
+            ?>
+        </h3>
+    </div>
+    <a href="../courses/courses.php">Placeholder Class Link</a>
 </main>
 
 <!--Includes the defined footer-->
