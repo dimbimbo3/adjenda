55e2866c954def39efb8f145162a6cfc0df8bb15
Dan Imbimbo <dimbimbo@student.sussex.edu>
2022-03-07 20:14:54 -0500

    Added retrieval of student roster and course info

--- course/course.php
+++ course/course.php
@@ -1,6 +1,9 @@
 <?php session_start(); //Resumes the session ?> 
 
 <?php
+require_once('../model/courseFunctions.php');
+require_once('../model/rosterFunctions.php');
+
 //retrieves the chosen action
 $action = filter_input(INPUT_POST, 'action');
 
@@ -15,9 +18,8 @@ if ($action == NULL)
 switch($action){
 	//displays the course
     case 'showCourse':
-        //$course = getCourseByID() using session courseNum
-        //$roster = getClassRoster() using session courseNum
-        //$lesson = getLesson() using session courseNum
+        $course = getCourseByID($_SESSION['courseID']);
+        $students = getStudentRosterByID($_SESSION['courseID']);
         include('courseDisplay.php');
         break;
 }
