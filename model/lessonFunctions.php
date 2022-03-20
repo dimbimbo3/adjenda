823ac4944317448fe7ddce0fd6fc4ddc1ca41dd6
Dan Imbimbo <dimbimbo@student.sussex.edu>
2022-03-20 18:05:22 -0400

    Added a lesson table function

---
+++ model/lessonFunctions.php
@@ -0,0 +1,27 @@
+<?php require_once('database.php'); ?>
+
+<?php
+// Checks if the current date is a lesson for the selected course
+function checkLessonDate($courseID, $currentDate) {
+    global $db;
+    $query = 'SELECT * FROM LESSONS
+                WHERE courseID = :courseID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':courseID', $courseID);
+    $statement->execute();
+    $lessons = $statement->fetchAll();
+    $statement->closeCursor();
+
+    foreach($lessons as $lesson){
+        if($currentDate == $lesson['ldate']){
+            $result = true;
+            break; //breaks out of the loop if found
+        }
+        else{
+            $result = false;
+        }
+    }
+
+    return $result;
+}
+?>
