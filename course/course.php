8a0c00484cf1a2a1494b665ae88ccb58fdbb6c54
Dan Imbimbo <dimbimbo@student.sussex.edu>
2022-03-29 21:48:37 -0400

    Alterations to time checking for attendance

--- course/course.php
+++ course/course.php
@@ -27,21 +27,21 @@ switch($action){
         break;
     //generates the attendance code for the current lesson
     case 'takeAttendance' :
+        $course = getCourseByID($_SESSION['courseID']);
         //*needs logic for checking that the current date aligns with one of the selected days for a given course, and that the current time falls within the start/end time for it*
         //(if a lesson is not happening at the current time then do not generate code and instead display message saying that a class is not in progress)
         if(checkLessonDate($_SESSION['courseID'], date("Y-m-d"))){
             //if the current date has a lesson, check that the time is within the range of the lesson
-            if(date('H:m:s') > $_SESSION('startTime') && date('H:m:s') < $_SESSION('endTime')){
+            if(strtotime(date('H:m:s')) >= strtotime($course['startTime']) && strtotime(date('H:m:s')) <= strtotime($course['endTime'])){
                 $_SESSION['attendanceCode'] = getAttendanceCode();
-                echo "<script> alert('Attendance code has been generated for the lesson.'); </script>";
+                echo "<script> alert('Attendance code has been generated for the current lesson.'); </script>";
             }
         }
         else{
-            echo "<script> alert('Class is not in progress: No code has been generated.'); </script>";
+            echo "<script> alert('NO CODE HAS BEEN GENERATED: The class is not in progress'); </script>";
         }
-        //*needs logic for displaying the code on the instructor's screen*
-        //(CONDITION WITHIN THE 'courseDisplay.php' FILE)
-        echo "<script> document.location='../course/course.php'; </script>"; //return to course page
+        
+        echo "<script> window.location='../course/course.php'; </script>"; //return to course page
         break;
     //downloads the selected attendance log
     case 'downloadLog' :
@@ -51,7 +51,7 @@ switch($action){
         //*needs logic for downloading the selected attendance log*
         //$selectedDate = $_POST['date'];
         //downloadLog($_SESSION['courseID'], $selectedDate)
-        echo "<script> document.location='../course/course.php'; </script>"; //return to course page
+        echo "<script> window.location='../course/course.php'; </script>"; //return to course page
         break;
 }
 
